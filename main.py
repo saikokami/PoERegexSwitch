cfc4da6eb1f1942029e5343e2f06cc810bff719b
Bennedikt Wältermann <bennedikt.waeltermann@web.de>
2023-08-15 08:30:44 +0200

    added all vendors

--- main.py
+++ main.py
@@ -6,9 +6,23 @@ import win32clipboard
 
 
 npc_patterns = {
-    "clarrisa": "ABC",
-    "hargan": "r-r-|-r-r|r-.-r",
-    "yeena": "b-b-g|b-g-b|g-b-b|nne|rint"
+    "nessa": "regexHere",  # Act 1
+    "tarkleigh": "regexHere",  # Act 1
+    "greust": "regexHere",  # Act 2
+    "yeena": "regexHere",  # Act 2
+    "clarissa": "regexHere",  # Act 3
+    "hargan": "regexHere",  # Act 3
+    "siosa": "regexHere",  # Act 3
+    "kira": "regexHere",  # Act 4
+    "petarus and vanja": "regexHere",  # Act 4
+    "lani": "regexHere",  # Act 5
+    "utula": "regexHere",  # Act 5
+    "bannon": "regexHere",  # Act 5
+    "bestel": "regexHere",  # Act 6
+    "lilly roth": "regexHere",  # Act 6
+    "helena": "regexHere",  # Act 7
+    "irasha": "regexHere",  # Act 9
+    "weylam roth": "regexHere",  # Act 10
 }
 
 def regexToClipBoard(text):
@@ -25,7 +39,7 @@ def ocr_loop():
         x1, y1, x2, y2 = (screen_width - 150) // 2, 0, (screen_width + 150) // 2, 35
         screenshot = ImageGrab.grab(bbox=(x1, y1, x2, y2))
         text = pytesseract.image_to_string(screenshot).lower().strip()
-        
+
         if text in npc_patterns:
             updated_text = regexToClipBoard(text)
             update_clipboard_win32(updated_text)
